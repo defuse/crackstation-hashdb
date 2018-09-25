38743a25266a8e3e7f8b0f9564b337f0a4598738
James Young <james@jamesyoung.digital>
2018-09-25 13:19:19 +1000

    Add test/createhashlists.php test, clean up lookup_hash.php

--- test/lookup_hash.php
+++ test/lookup_hash.php
@@ -16,26 +16,56 @@ function menu() {
 	return $hash;
 }
 
-$hash_algorithm = $argv[1];
-$lookup = new LookupTable("test-index-files/test-words-$hash_algorithm.idx", "test/words.txt", $hash_algorithm);
+function crack_hashes($hasher, $lookup, $hash_algorithm, $fname, $interactive = false) {
 
-$hasher = MoreHashAlgorithms::GetHashFunction($hash_algorithm);
-
-if (count($argv) === 3) {
+	if (!$interactive) {
+		
+		// Non-interactive mode
+		$fp = fopen($fname, 'r');
 	
-	// Non-interactive mode
-	$fp = fopen($argv[2], 'r');
+		if ($fp === false) {
+    		echo "Error opening " . $fname . "\n";
+    		exit(1);
+		}
+
+		while (($to_crack = fgets($fp)) !== false) {
+
+			$to_crack = rtrim($to_crack, "\r\n");
+
+			// Ugly code duplication... fixme pls
+			$results = $lookup->crack($to_crack);
+    
+    		if (count($results) > 0) {
+    			echo $cracked = $to_crack . ":" . $results[0]->getPlaintext() . "\n";
+    		}
+    		else {
+    			echo $cracked = "Nothing for " . $hash_algorithm . ":" . $to_crack . "\n";
+    		}
+
+    		// Partial match (first 8 bytes, 16 hex chars).
+    		$to_crack = substr($to_crack, 0, 16);
+    		$results = $lookup->crack($to_crack);
+
+    		if (count($results) > 0) {
+    			echo $cracked = $to_crack . ":" . $results[0]->getPlaintext() . " (partial match)\n";
+    		}
+    		else {
+    			echo $cracked = "Nothing for " . $hash_algorithm . ":" . $to_crack . "\n";
+    		}
+		}
+
+		fclose($fp);
+
+		exit(0);
+	} // non interactive mode
 	
-	if ($fp === false) {
-    	echo "Error opening " . $argv[2] . "\n";
-    	exit(1);
-	}
 
-	while (($to_crack = fgets($fp)) !== false) {
+	// interactive mode
+	echo "To exit type: quit\n";
 
-		$to_crack = rtrim($to_crack, "\r\n");
+	$to_crack = "";
+	while ( ($to_crack = menu()) !== "quit") {
 
-		// Ugly code duplication... fixme pls
 		$results = $lookup->crack($to_crack);
     
     	if (count($results) > 0) {
@@ -44,8 +74,6 @@ if (count($argv) === 3) {
     	else {
     		$cracked = "Nothing for " . $hash_algorithm . ":" . $to_crack . "\n";
     	}
-
-    	echo $cracked;
 
     	// Partial match (first 8 bytes, 16 hex chars).
     	$to_crack = substr($to_crack, 0, 16);
@@ -59,40 +87,22 @@ if (count($argv) === 3) {
     	}
 
     	echo $cracked;
-	}
 
-	fclose($fp);
-
-	exit(0);
+	} // end interactive mode, user types "quit"
 }
 
-echo "To exit type: quit\n";
+$hash_algorithm = $argv[1];
+$lookup = new LookupTable("test-index-files/test-words-$hash_algorithm.idx", "test/words.txt", $hash_algorithm);
 
-$to_crack = "";
-while ( ($to_crack = menu()) !== "quit") {
+$hasher = MoreHashAlgorithms::GetHashFunction($hash_algorithm);
 
-	$results = $lookup->crack($to_crack);
-    
-    if (count($results) > 0) {
-    	$cracked = $to_crack . ":" . $results[0]->getPlaintext() . "\n";
-    }
-    else {
-    	$cracked = "Nothing for " . $hash_algorithm . ":" . $to_crack . "\n";
-    }
+// Set to true unless we are given a file name for non-interactive mode below
+$interactive_mode = true;
+$file_name = "";
 
-    echo $cracked;
-
-    // Partial match (first 8 bytes, 16 hex chars).
-    $to_crack = substr($to_crack, 0, 16);
-    $results = $lookup->crack($to_crack);
-
-    if (count($results) > 0) {
-    	$cracked = $to_crack . ":" . $results[0]->getPlaintext() . " (partial match)\n";
-    }
-    else {
-    	$cracked = "Nothing for " . $hash_algorithm . ":" . $to_crack . "\n";
-    }
-
-    echo $cracked;
-
+if (count($argv) === 3) {
+	$interactive_mode = false;
+	$file_name = $argv[2];
 }
+
+crack_hashes($hasher, $lookup, $hash_algorithm, $file_name, $interactive_mode);
